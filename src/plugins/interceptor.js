907a94be8161f598c06036446d544ba623564e4f
Apfirebolt <aspper20@gmail.com>
2025-03-24 23:16:34 +0530

    added production url

--- src/plugins/interceptor.js
+++ src/plugins/interceptor.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/'
+    // for dev
+    // baseURL: 'http://localhost:3000/api/'
+    // for production
+    baseURL: 'https://next15-auth-boilerplate.vercel.app/api/'
 });
 
 export default axiosInstance;
